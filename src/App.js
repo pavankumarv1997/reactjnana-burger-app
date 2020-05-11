dc1e6db6f53dfcdf4867c5236e11af753f3a969e
pavankumarv1997 <pavankumarv287.pk@gmail.com>
2020-05-12 01:50:30 +0530

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return <div>Hello from React</div>;
+class App extends Component {
+  render() {
+    return <div className="App">hello from App</div>;
+  }
 }
 
 export default App;
